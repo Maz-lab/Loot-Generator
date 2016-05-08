5c83405b725e8f46851942c3e9b715c314079d83
Maz-lab <mazariegosnx@gmail.com>
2016-05-08 13:06:28 -0400

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,8 @@
+/*
+* This loot generator will create gear with randomized names, types and affixes.
+* We will use txt files that contain the possible names types and affixes and 
+* have the code read from them.
+* We will accomplish the random part by using the random function from the math library.
+* When an item is created we will prompt the user if they want to save it, if they do we will write the items
+* characteristics to a new txt file meant to hold these results.
+/*
